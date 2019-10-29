4e25eb3e843bfbe70483aa7cd28a6f0fb8fc911f
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2019-10-29 13:43:10 +0330

    Updated settings for created apps

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rate.apps.ApiConfig',
+    'user.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
