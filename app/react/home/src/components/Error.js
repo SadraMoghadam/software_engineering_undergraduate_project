2464d601d8cca00500aba20e648348e9e926f562
Sadra Moghadam <sadra_h_m@outlook.com>
2019-11-07 03:39:58 +0330

    basic home added

---
+++ app/react/home/src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+const Error = () => {
+    return (
+       <div className="error">
+          <p>Error: Page does not exist!</p>
+       </div>
+    );
+}
+ 
+export default Error;
---
+++ app/react/home/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+ 
+const Navigation = () => {
+    return (
+        <div className="header">
+           <h1 id="ostad-rate">Ostad Rate</h1>
+        </div>
+    );
+}
+ 
+export default Navigation;
