9fdcd2c7e5f0b86bf3c004134135aa0e48e49909
Sadra Moghadam <sadra_h_m@outlook.com>
2019-12-03 03:26:00 +0330

    university rate completed

--- app/react/my-app/src/App.js
+++ app/react/my-app/src/App.js
@@ -4,6 +4,8 @@ import Home from './components/Home';
 import Register from './components/Register';
 import Logintoregister from './components/Logintoregister';
 import RegisterOstad from './components/RegisterOstad';
+import ProfessorRate from './components/ProfessorRate';
+import UniversityRate from './components/UniversityRate';
 import './App.css';
 import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 
@@ -17,6 +19,8 @@ function App() {
         <Route exact path="/Logintoregister" component={Logintoregister}/>
         <Route exact path="/Register" component={Register}/>
         <Route exact path="/RegisterOstad" component={RegisterOstad}/>
+        <Route exact path="/ProfessorRate" component={ProfessorRate}/>
+        <Route exact path="/UniversityRate" component={UniversityRate}/>
       </Switch>
     </Router>
     
--- app/react/my-app/src/components/Home.js
+++ app/react/my-app/src/components/Home.js
@@ -1,19 +1,56 @@
-import logo from '../ostadrate.png';
+
 import React, {Component} from 'react';
 import '../newCss.css';
-import Register from './Register';
 import Header from './Header';
-import Logintoregister from './Logintoregister';
 import { Redirect } from 'react-router-dom';
 
 class Home extends Component {
+
+    state = {
+        redirectProfessorRate: false,
+        redirectUniRate: false
+      } 
+    
+        setRedirectProfessorRate = () => {
+          this.setState({
+            redirectProfessorRate: true
+          })
+        }
+
+        renderRedirectProfessorRate = () => {
+          if (this.state.redirectProfessorRate) {
+            return <Redirect to='./ProfessorRate' />
+          }
+        }
+
+        setRedirectUniRate = () => {
+            this.setState({
+              redirectUniRate: true
+            })
+          }
+  
+          renderRedirectUniRate = () => {
+            if (this.state.redirectUniRate) {
+              return <Redirect to='./UniversityRate' />
+            }
+          }
+
     render() {
         return(
             <div className="Home">
                 <Header/>
                 <div className="allBackground">
+                <div className="rateButton" onClick={this.setRedirectProfessorRate}>
+                    {this.renderRedirectProfessorRate()}
+                    امتیازدهی استاد
+                </div>
+                <div className="rateButton" onClick={this.setRedirectUniRate}>
+                    {this.renderRedirectUniRate()}
+                    امتیازدهی دانشگاه
+                </div>
                 </div>
             </div>
+            
         );
     }
 }
